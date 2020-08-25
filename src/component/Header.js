f8f1f74fe15e6713eec7bd6a1013c95588377913
Dinesh <dineshmj82@gmail.com>
2020-08-25 19:18:04 +0530

    button

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
     return (
         <div className="w3-container w3-center w3-teal"> 
-            <h2>Account</h2>
+            <h2 className="p-2">Account</h2>
         </div>
     )
 }
